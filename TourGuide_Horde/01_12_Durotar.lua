cd7d5075cfc71efd44ab402a6be2e1de5f18e676
Blueager2 <112143947+Blueager2@users.noreply.github.com>
2022-09-19 21:58:52 -0700

    Add files via upload

---
+++ TourGuide_Horde/01_12_Durotar.lua
@@ -0,0 +1,345 @@
+TourGuide:RegisterGuide("Goblin Hardcore (1-10)", "The Barrens (12-15)", "Horde", function()
+return [[
+
+N ##### SECTION 1: Stonetalon Mountains ##### (Check box to continue)
+N Hover over to see note 1/3 |N|This is a completionist guide for hardcore players. I try to optimize the quest routes while avoiding unnecessary risks and experiencing as much of the game as possible. Optional sections will be marked.
+N See note 2/3 |N|While following the guide, check notes to see extra information.|
+N See note 3/3 |N|The difficulty in this section ramps up quickly hence the grinding sessions. Feel free to skip them.|
+A Stay awhile and listen... |N|(39.3, 29.3)| |QID|80388| |Z|Stonetalon Mountains|
+T Stay awhile and listen... |QID|80388|
+K (Optional) Kill Whirling Whizz-Bots until you have 10c
+B (Optional) Buy [Glyph of the Turtle] from Scribe Jardis |N|(38.3, 34.4)| |Z|Stonetalon Mountains|
+B (Optional) Buy [Glyph of Exhaustion]
+U (Optional) Use [Glyph of the Turtle] |U|51230| 
+U (Optional) Use [Glyph of Exhaustion] |U|50520|
+
+A The Other White Mech |N|(37.5, 33.0)| |QID|80104| |Z|Stonetalon Mountains|
+A A New Ad-Venture |N|(38.2, 33.1)| |QID|80100| |Z|Stonetalon Mountains|
+A Garbage Man |N|(38.3, 32.3)| |QID|80120| |Z|Stonetalon Mountains|
+C Garbage Man |QID|80120|
+C A New Ad-Venture |QID|80100|
+C The Other White Mech |QID|80104|
+
+T The Other White Mech |N|(37.5, 33.0)| |QID|80104| |Z|Stonetalon Mountains|
+A Venture Vultures |QID|80101|
+T A New Ad-Venture |N|(38.2, 33.1)| |QID|80100| |Z|Stonetalon Mountains|
+A Grease-stained Letter |QID|80115| |C|Warrior|
+A Leather-covered Letter |QID|80117| |C|Hunter|
+A Ooze-covered Letter |QID|80116| |C|Rogue|
+A Fancy Letter |QID|80118| |C|Mage|
+A Awful-looking Letter |QID|80119| |C|Warlock|
+A Green Versus Green |QID|80102|
+T Garbage Man |N|(38.3, 32.3)| |QID|80120| |Z|Stonetalon Mountains|
+N Vendor and repair (See Note) |N|I won't be mentioning opportunities to vendor anymore unless there's something you should save.|
+T Grease-stained Letter |N|(38.1, 30.3)| |QID|80115| |C|Warrior| |Z|Stonetalon Mountains|
+T Leather-covered Letter |N|(39.2, 31.4)| |QID|80117| |C|Hunter| |Z|Stonetalon Mountains|
+T Ooze-covered Letter |N|(39.1, 29.6)| |QID|80116| |C|Rogue| |Z|Stonetalon Mountains|
+T Fancy Letter |N|(38.2, 34.4)| |QID|80118| |C|Mage| |Z|Stonetalon Mountains|
+T Awful-looking Letter |N|(39.4, 29.4)| |QID|80119| |C|Warlock| |Z|Stonetalon Mountains|
+N Train skills
+
+C Green Versus Green |N|The Environmentalists are stealthed so use caution.| |QID|80102|
+C Venture Vultures |N|They can be found in the hills around the safe area| |QID|80101|
+K Grind until 30% from Level 4
+
+T Green Versus Green |N|(38.2, 33.1)| |QID|80102| |Z|Stonetalon Mountains|
+A Risen Oilblazes |QID|80103|
+T Venture Vultures |N|(37.5, 33.0)| |QID|80101| |Z|Stonetalon Mountains|
+N Train skills
+A This Is In My Contract |QID|80121| |C|Warlock|
+
+C This Is In My Contract |N|Drops off Animated Shredders to the South-west| |QID|80121| |C|Warlock|
+T This Is In My Contract |N|(39.4, 29.4)| |QID|80121| |C|Warlock| |Z|Stonetalon Mountains|
+
+C Risen Oilblazes |N|(34, 36)| |QID|80103| |Z|Stonetalon Mountains|
+K (Optional) Grind until 25% from Level 5
+
+A Ventured Too Far |N|Started by the Oil Scorched Note in your inventory| |O| |U|80114| |QID|80105|
+T Risen Oilblazes |N|(38.2, 33.1)| |QID|80103| |Z|Stonetalon Mountains|
+T Ventured Too Far |QID|80105|
+A The Grumbling Grove |QID|80106|
+
+C The Grumbling Grove |QID|80106| |N|(31.5, 38)| |Z|Stonetalon Mountains|
+K (Optional) Grind until 10% from Level 6
+
+T The Grumbling Grove |N|(38.2, 33.1)| |QID|80106| |Z|Stonetalon Mountains|
+A Shadow On The Plateau |QID|80107|
+N Train skills
+
+N See note |N|Consider grouping up for this next quest if you're a Hunter||N|
+C Shadow On The Plateau |N|(31.7, 39.6)| |QID|80107| |Z|Stonetalon Mountains|
+H Use Hearth
+
+T Shadow On The Plateau |N|(38.2, 33.1)| |QID|80107| |Z|Stonetalon Mountains|
+A Me Not Any Kind Of Orc |QID|80108|
+N Travel to Durotar |N|Use the "FM Acquisition, Blastentom's Property" on the landing pad at (38.9, 34.1)| |Z|Stonetalon Mountains|
+T Me Not Any Kind Of Orc |N|(50.1, 35.9)| |QID|80108| |Z|Durotar|
+A Zug-zug Or Somethin' |QID|80109|
+N See Note 1/2 |N|From now on, kill any Dire Mottled Boars you see until you have [Chunk of Boar Meat] x25
+N See Note 2/2 |N|As you quest, save [Scorpid Stinger] x5
+A Carry Your Weight |QID|791| |N|From watch tower northwest of town (49.88, 40.31)| |Z|Durotar|
+h Set hearth to Razor Hill |N|(51.5, 41.6)| |Z|Durotar|
+N Pick up any professions you want
+N Buy Blood Pact from the Demon Trainer |N|(54.7, 41.5)| |C|Warlock| |Z|Durotar|
+
+N ##### SECTION 2 (OPTIONAL): Valley of Trials #####
+N See note |N|If you don't care to do the Orc and Troll starting area, run to Sen'jin Village and skip to section 3.|
+R Run to Valley of Trials |N|(44.6, 68.6)| |Z|Durotar|
+A Lazy Peons |QID|5441| 
+A Vile Familiars |N|(42.8, 69.0)| |QID|792| |Z|Durotar|
+A Cutting Teeth |N|(42.13, 68.37)| |QID|788| |Z|Durotar|
+
+C Lazy Peons |QID|5441| |U|16114| |N|Run around and beat the sleeping peons. If you head south west of the questgiver you can get most of them.|
+C Vile Familiars |QID|792| |N|Kill familiars outside the cave at the north end of the valley.|
+C Cutting Teeth |QID|788|
+A Sarkoth (Part 1) |QID|790| |N|Western Valley of Trials, just north of the Den (40.64, 62.61)| |Z|Durotar|
+C Sarkoth (Part 1) |QID|790| |N|South of the questgiver, up the hill (40.43, 67.03)| |Z|Durotar|
+T Sarkoth (Part 1) |N|(40.64, 62.61)| |QID|790| |Z|Durotar|
+A Sarkoth (Part 2) |QID|804|
+
+T Lazy Peons |N|(44.6, 68.6)| |QID|5441| |Z|Durotar|
+A Thazz'ril's Pick |QID|6394|
+T Vile Familiars |N|(42.8, 69.0)| |QID|792| |Z|Durotar|
+A Burning Blade Medallion |QID|794|
+A Stalking the Felstalkers |QID|60147|
+A Vile Familiars |N|(42.6, 69.0)| |QID|1485| |C|Warlock| |Z|Durotar|
+T Cutting Teeth |QID|788| |N|Back at the Den (42.13, 68.37)| |Z|Durotar|
+A Sting of the Scorpid |QID|789|
+T Sarkoth (Part 2) |QID|804|
+A Glyphic Tablet |QID|3086| |C|Mage|
+A Tainted Parchment |QID|3090| |C|Warlock|
+T Glyphic Tablet |N|(42.5, 69.0)| |QID|3086| |C|Mage| |Z|Durotar|
+T Tainted Parchment |N|(40.6, 68.5)| |QID|3090| |C|Warlock| |Z|Durotar|
+A Galgar's Cactus Apple Surprise |N|(42.6, 67.2)| |QID|4402| |Z|Durotar|
+
+C Thazz'ril's Pick |QID|6394| |N|In the cave at the north end of the valley. Go straight ahead at the fork (43.76, 53.77)| |Z|Durotar|
+C Burning Blade Medallion |QID|794| |N|Go back to the fork and take the northern path (42.69, 53.03)| |Z|Durotar|
+C Stalking the Felstalkers |QID|60147|
+C Vile Familiars |QID|1485| |C|Warlock|
+C Sting of the Scorpid |QID|789|
+C Galgar's Cactus Apple Surprise |QID|4402|
+
+T Galgar's Cactus Apple Surprise |N|(42.6, 67.2)| |QID|4402| |Z|Durotar|
+T Sting of the Scorpid |N|(42.13, 68.37)| |QID|789| |Z|Durotar|
+T Vile Familiars |N|(42.6, 69.0)| |QID|1485| |C|Warlock| |Z|Durotar|
+T Stalking the Felstalkers |N|(42.8, 69.0)| |QID|60147| |Z|Durotar|
+T Burning Blade Medallion |QID|794|
+A Report to Sen'jin Village |QID|805|
+A Professor Malkovich |N|(41.3, 68.0)| |QID|50000| |C|Rogue| |Z|Durotar|
+T Thazz'ril's Pick |N|(44.6, 68.6)| |QID|6394| |Z|Durotar|
+
+R Run to Sen'jin Village |N|Take the path southeast to Sen'jin Village (55.96, 74.67)| |Z|Durotar|
+T Report to Sen'jin Village |QID|805|
+
+N ##### SECTION 3: Sen'jin Village #####
+A Report to Orgnil |QID|823|
+A Minshina's Skull |QID|808|
+A Zalazane |QID|826|
+A A Solvent Spirit |N|(55.9, 74.4)| |QID|818| |Z|Durotar|
+A Study to Survive |N|(56.7, 73.7)| |QID|810| |Z|Durotar|
+N See Note |N|From now on, kill any Clattering Scorpids you see until you have [Small Scorpid Carapace] x6|
+A Pinch of Salt |N|If you are not level 6, come back when you are. You could really use the experience. (53.3, 81.6)| |QID|80384| |Z|Durotar|
+
+C Pinch of Salt |QID|80384|
+C A Solvent Spirit |QID|818|
+T Pinch of Salt |N|(53.3, 81.6)| |QID|80384| |Z|Durotar|
+C Study to Survive |QID|810|
+
+N See note |N|Save [Scorpid Stinger] x5|
+T A Solvent Spirit |N|(55.9, 74.4)| |QID|818| |Z|Durotar|
+A What Do You Rely On? |QID|820| 
+T Study to Survive |N|(56.7, 73.7)| |QID|810| |Z|Durotar|
+A Unity is Strength |QID|811|
+N Train skills |C|Mage|
+
+A Thwarting Kolkar Aggression |QID|786| |N|Lar Prowltusk patrols nearby (54,76)| |Z|Durotar|
+
+N See note |N|There is a rare centuar, Warlord Kolkanis, that spawns in the next area at the northern camp. We will come back to him later.|
+C Thwarting Kolkar Aggression |QID|786| |N|West of Sen'jin at Kolkar Crag, each camp has an item to burn (49.84, 81.18), (46.22, 78.97), (47.68, 77.44).| |Z|Durotar|
+C Carry Your Weight |QID|791| |N|Keep killing centaurs for the scraps.|
+
+T Thwarting Kolkar Aggression |QID|786| |N|(54,76)| |Z|Durotar|
+
+N ##### SECTION 4: Razor Hill and Sparkwater Port #####
+A A Peon's Burden |QID|2161| |N|East out of the Valley of Trials (51.98, 68.27)| |Z|Durotar|
+C Zug-zug Or Somethin' |QID|80109|
+R Run to Razor Hill |N|Kill boars on the way until you have [Chunk of Boar Meat x25 (51.5, 41.6)| |Z|Durotar|
+T A Peon's Burden |QID|2161|
+B Buy [Mutton Chop] x5
+A No More-pid! |N|(51.3, 41.5)| |QID|60139| |Z|Durotar|
+T Carry Your Weight |N|(49.88, 40.31)| |QID|791| |T|  |Z|Durotar|
+T Zug-zug Or Somethin' |N|(51.1, 42.5)| |QID|80109| |Z|Durotar|
+A Green Goes Red |QID|80110| 
+A Work for Food |QID|814|
+C Work for Food |QID|814|
+T Work for Food |QID|814|
+A Break a Few Eggs |QID|815| |T| 
+T Green Goes Red |N|(52.0, 43.4)| |QID|80110| |Z|Durotar|
+A Vanquish the Betrayers |QID|784| |T|
+T Report to Orgnil |QID|823| |N|(52.2, 43.2)| |T| |Z|Durotar|
+A Dark Storms |QID|806| |T|
+A Durotar Scouring |QID|807|
+A Supplies to the Port! |N|(53.0, 42.0)| |QID|55002| |Z|Durotar|
+A Red Skies of Durotar |N|(53.1, 41.5)| |QID|40298| |Z|Durotar|
+N Find a [Bone Chew Toy] |N|Very small on the ground. (51.5, 41.6), (53.1, 41.9)| |Z|Durotar|
+C Red Skies of Durotar |QID|40298|
+T Red Skies of Durotar |N|(53.1, 41.4)| |QID|40298| |Z|Durotar|
+N Train skills
+
+R Run to Sparkwater Port |N|Kill Armored Scorpids on the way (58.6, 25.7)| |Z|Durotar|
+T Supplies to the Port! |QID|55002|
+A Adding a Little Sting |N|Behind the inn (59.1, 24.6)| |QID|55004| |Z|Durotar|
+C Adding a Little Sting |QID|55004|
+T Adding a Little Sting |QID|55004|
+
+N ##### SECTION 5 (OPTIONAL): Underwater #####
+N See Note |N|The next two quests involve underwater mobs. It is good advice to avoid water. If you get disconnected while underwater, you'll drown. So either pull the mobs to the surface, take the risk, or skip this section.|
+A The Oil Stops Flowing |N|Outside the port (59.9, 25.5)| |QID|55001| |Z|Durotar|
+A Razlik's Tools |N|(59.5, 24.6)| |QID|55000| |Z|Durotar|
+
+C Razlik's Tools |N|On the top level of the platform east of the port. (62.7, 23.8)| |QID|55000| |Z|Durotar|
+C The Oil Stops Flowing |QID|55001|
+
+T The Oil Stops Flowing |N|(59.9, 25.5)| |QID|55001| |Z|Durotar|
+T Razlik's Tools |N|(59.5, 24.6)| |QID|55000| |Z|Durotar|
+
+N ##### SECTION 6: Harpies and back to Razor Hill #####
+N Bank unneeded items (Save [Copper Ore] x10 if you have it) |N|(58.4, 27.5)| |Z|Durotar|
+A A New Power Source |N|(57.4, 25.8)| | |QID|55003| |Z|Durotar|
+A Missing Worker! |N|(56.2, 27.1)| |QID|55044| |Z|Durotar|
+T Missing Worker! |N|(56.0, 25.4)| |QID|55044| |Z|Durotar|
+
+N See Note |N|The next quest requires [Copper Ore] x10. Skip if you don't have it.|
+A Ore on Time |QID|55045|
+T Ore on Time |QID|55045|
+
+A Winds in the Desert |N|(46.42, 22.94)| |QID|834| |Z|Durotar|
+A Triggered! |QID|60120| 
+C Triggered! |N|The Ammo Crates are in the west canyon, supplies can be found in the east and west canyons.(49,33)| |QID|60120| |Z|Durotar|
+C Winds in the Desert |QID|834| |N|(49,33), (50,25)| |Z|Durotar|
+
+T Triggered! |N|(46.4, 23.0)| |QID|60120| |Z|Durotar|
+T Winds in the Desert |QID|834|
+A Securing the Lines |QID|835|
+
+A Lost But Not Forgotten |QID|816| |N|Northwest of Razor Hill at Tor'kren Farm (43.10, 30.30)| |Z|Durotar|
+C No More-pid! |N|(45,34)| |QID|60139| |Z|Durotar|
+C Unity is Strength |N|(45,34)| |QID|811| |Z|Durotar|
+N See note 1/2 |N|If you have completed all optional sections, you should be close to level 10. Grind until 5% from Level 10.|
+N See note 2/2 |N|If you are not close to level 10, you will need to either grind or skip the class quest portion of this guide and do them on your own later.|
+
+T No More-pid! |N|(51.3, 41.5)| |QID|60139| |Z|Durotar|
+A The Hunter's Path |N|This quest is an optional breadcrumb quest (67.9, 17.8)| |QID|80330| |C|Hunter| |Z|Orgrimmar|
+A Speak with Un'thuwa |N|(39.2, 86.3)| |QID|1883| |Z|Orgrimmar| |C|Mage|
+
+N Check mail for Slow and Steady reward
+N Train Skills
+N See note |N|I am choosing to delay completion of some class quests until later. If you want to do them now, feel free to do so before continuing.|
+A Veteran Uzzek |QID|1505| |C|Warrior|
+A Therzok |QID|1859| |C|Rogue|
+T The Hunter's Path |N|(53.2, 43.6)| |QID|80330| |C|Hunter| |Z|Durotar|
+A Taming the Beast (Part 1) |QID|80331| |C|Hunter|
+C Taming the Beast (Part 1) |QID|80331| |C|Hunter|
+T Taming the Beast (Part 1) |N|(53.2, 43.6)| |QID|80331| |C|Hunter| |Z|Durotar|
+A Taming the Beast (Part 2) |QID|80332| |C|Hunter|
+C Taming the Beast (Part 2) |QID|80332| |C|Hunter| |N|Surf Crawler (59, 28)| |Z|Durotar|
+T Taming the Beast (Part 2) |N|(53.2, 43.6)| |QID|80332| |C|Hunter| |Z|Durotar|
+A Taming the Beast (Part 3) |QID|80333| |C|Hunter|
+C Taming the Beast (Part 3) |QID|80333| |C|Hunter| |N|Armored Scorpid (49, 37)| |Z|Durotar|
+T Taming the Beast (Part 3) |N|(53.2, 43.6)| |QID|80333| |C|Hunter| |Z|Durotar|
+A Training the Beast |QID|80334| |C|Hunter|
+T Training the Beast |N|(66.0, 18.6)| |QID|80334| |C|Hunter| |Z|Orgrimmar|
+P Tame Venomtail Scorpid |N|Outside city gates| |C|Hunter|
+
+N ##### SECTION 7: Tiragarde Keep and Echo Isles #####
+K Lieutenant Benedict and loot his key |N|Southeast at Tiragarde Keep. Lieutenant Benedict is inside, upstairs (59.67, 58.36).| |Z|Durotar|
+A The Admiral's Orders (Part 1) |U|4881| |QID|830| |N|Open the box on the ramparts upstairs from Benedict (59.32, 57.67).| |Z|Durotar|
+C Vanquish the Betrayers |QID|784| |N||
+
+R Run to Sen'jin Village |N|(56.0, 73.9)| |Z|Durotar|
+A Practical Prey |QID|817|
+T Unity is Strength |N|(56.7, 73.7)| |QID|811| |Z|Durotar|
+T Speak with Un'thuwa |N|(56.3, 75.1)| |QID|1883| |Z|Durotar| |C|Mage|
+A Ju-Ju Heaps |QID|1884| |C|Mage|
+
+N Loot raptor eggs and kill panthers as you continue
+N See note |N|There's a quest in Orgrimmar for [Small Leather Collar] x5. But also requires [Malachite] x5 and [Light Leather] x5. So save these if you want/are able to do that quest.|
+C Ju-Ju Heaps |N|There are two in these huts. They don't take long to respawn. (68,85)| |QID|1884| |Z|Durotar| |C|Mage|
+C Minshina's Skull |QID|808| |N|Head over to the Echo Isles to the main island. Near the southern troll camp on the hill near Zalazane, in the ritual circle (67.51, 87.72).| |Z|Durotar|
+C Zalazane |QID|826| |N|Zalazane is in the troll camp near Minshina's skull (67.05, 87.15).| |Z|Durotar|
+A Zalazane's Apprentice |N|From [Shabby Letter] dropped off Hexed Trolls and Voodoo Trolls| |QID|80399| |U|59990|
+C Break a Few Eggs |QID|815| |N|Look for raptor eggs around the edges of the main island and on the other islands.|
+C Practical Prey |QID|817|
+
+N ##### SECTION 8 (OPTIONAL): Underwater #####
+H Hearth to Razor Hill
+T Break a Few Eggs |QID|815| |T| 
+A Higher Stakes |N|(51.1, 42.5)| |QID|50305| |Z|Durotar|
+T Vanquish the Betrayers |N|(52.0, 43.4)| |QID|784| |T| |Z|Durotar|
+A From The Wreckage.... |QID|825| |T| |NODEBUG|
+T The Admiral's Orders (Part 1) |QID|830| |T|
+A The Admiral's Orders (Part 2) |QID|831| |T| |Z|Durotar|
+A Oil-Stained Gold |N|(53.2, 43.4)| QID|70051| |Z|Durotar|
+C From The Wreckage.... |QID|825| |N|East of Razor Hill, off the coast (62,50).| |NODEBUG| |Z|Durotar|
+C Oil-Stained Gold |N|(62, 50)| QID|70051| |Z|Durotar|
+
+N ##### SECTION 9: Sen'jin Village #####
+C Zalazane's Apprentice |N|(61.5, 66.3)| |QID|80399| |Z|Durotar|
+
+T Minshina's Skull |QID|808| |N|Back at Sen'jin Village (55.95, 74.82)| |Z|Durotar|
+T Zalazane |QID|826|
+A Zalazane's Fall |QID|40389|
+N Save quest reward |N|You'll need the "Faintly Glowing Skull" for later quest "Burning Shadows"|
+T Zalazane's Apprentice |QID|80399|
+T Practical Prey |N|(56.0, 73.9)| |QID|817| |Z|Durotar|
+T Ju-Ju Heaps |N|(56.3, 75.1)| |QID|1884| |Z|Durotar| |C|Mage|
+
+N ##### SECTION 10 (OPTIONAL): Warlord Kolkanis (Rare) #####
+N See Note |N|This section involves killing a rare spawn. My longest wait is an hour. If you see his corpse, you might want to do something else.|
+K Kill Warlord Kolkanis |N|(47.9, 77.3)| |Z|Durotar|
+A Aggression's End |N|From Warlord Kolkanis' Head| |QID|40390|
+
+T Aggression's End |N|(55.9, 74.7)| |QID|40390| |Z|Durotar|
+A A Hunt for Honor |QID|40391|
+
+N ##### SECTION 11: Razor Hill and more Harpies #####
+R Run or Hearth to Razor Hill
+N See note |N|If you skipped the underwater section, turn in "Vanquish the Betrayers", "The Admiral's Orders", and "Break a Few Eggs." Take the followups "Higher Stakes" and "The Admiral's Orders (Part 2)."|
+T Oil-Stained Gold |N|Optional underwater quest (53.2, 43.4)| |T| |NODEBUG| |Z|Durotar|
+A A Tusken Affair |N|(52.6, 42.6)| |QID|80300| |Z|Durotar|
+T From The Wreckage.... |QID|825| |N|Optional underwater quest (51.90, 43.49)| |T| |NODEBUG| |Z|Durotar|
+
+C Securing the Lines |QID|835| |N|Head into eastern Razorwind Canyon, to the cave at the south end. Head through that, then back north up the Drygulch Ravine. (54,25)| |Z|Durotar|
+T Securing the Lines |QID|835| |N|Back at Rezlak (46.46, 22.92)| |Z|Durotar|
+A The Mail Must Go Through |QID|60121|
+
+N ##### SECTION 12: Orgrimmar #####
+A Need for a Cure |QID|812| |N|(41.6, 18.2)| |Z|Durotar|
+N Don't worry about the quest timer
+A Fashion Demands Sacrifices |N|(51.0, 73.7)| | |QID|60030| |Z|Orgrimmar|
+N If you don't have the materials, just abandon this quest
+T Fashion Demands Sacrifices |QID|60030|
+T The Mail Must Go Through |N|(54.3, 67.8)| |QID|60121| |Z|Orgrimmar|
+A Out For Delivery |QID|60122|
+A Quark's Shack |QID|80305|
+A Gadgetzan Times: READ ALL ABOUT IT! |N|(53.6, 65.6)| |QID|40457| |Z|Orgrimmar|
+T Gadgetzan Times: READ ALL ABOUT IT! |QID|40457| 
+A Gadgetzan Times: BREAKING NEWS! |QID|50315|
+T Gadgetzan Times: BREAKING NEWS! |QID|50315|
+T Out For Delivery |N|(52.1, 62.2)| |QID|60122| |Z|Orgrimmar|
+N Bank unneeded items (See Note) |N|You can bank [Faintly Glowing Skull]|
+N Take [Light Leather] x16 out of the bank if you have it.
+B Buy [Coarse Thread] x4 from Borya if you also have [Light Leather] x16 |N|(63.0, 51.5)| |Z|Orgrimmar|
+T Quark's Shack |N|(68.5, 25.8)| |QID|80305| |Z|Orgrimmar|
+A What's Yours is Ours |QID|80306|
+C What's Yours is Ours |N|Talk to Agne and gamble 1s| |QID|80306|
+T What's Yours is Ours |QID|80306|
+T Zalazane's Fall |N|(34.3, 36.3)| |QID|40389| |Z|Orgrimmar|
+T The Admiral's Orders (Part 2) |QID|831| |N|(32.3, 35.8)| |T| |Z|Orgrimmar|
+T Therzok |N|(42.7, 53.6)| |QID|1859| |C|Rogue| |Z|Orgrimmar|
+A Finding the Antidote |QID|813| |N|Down in the Cleft of Shadow (47.2, 53.6)| |T| |Z|Orgrimmar|
+T A Tusken Affair |N|(38.5, 86.2)| |QID|80300| |Z|Orgrimmar|
+N See note |N|Thank you for testing out this guide. Please feel free to offer any suggestions on the Turtle WoW forum post or on discord @Aeghas
+N ##### END OF GUIDE #####
+]]
+end)
